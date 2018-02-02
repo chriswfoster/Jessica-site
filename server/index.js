e6ffa09f3f0676f58576872c022fc92eb78ef097
ChriswFoster <chriswfoster@gmail.com>
2018-02-01 20:22:34 -0600

    Added auth0 content

--- server/index.js
+++ server/index.js
@@ -9,6 +9,79 @@ app.use(express.static(`${__dirname}/../build`))
 const port = 3001
 app.use(json())
 
+
+
+app.use(
+  session({
+    secret,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+
+massive(connectionString)
+  .then(db => app.set("db", db))
+  .catch(console.log);
+
+app.use(json());
+app.use(cors());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new Auth0Strategy(
+    {
+      domain,
+      clientID,
+      clientSecret,
+      callbackURL: "/login"
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+      app
+        .get("db")
+        .getUserByAuthId(profile.id)
+        .then(response => {
+          if (!response[0]) {
+            app
+              .get("db")
+              .createUserByAuth([profile.id, profile.displayName])
+              .then(created => {
+                return done(null, created[0]);
+              });
+          } else {
+            return done(null, response[0]);
+          }
+        });
+    }
+  )
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+app.get("/login",
+  passport.authenticate("auth0", {
+    successRedirect: "/"
+  })
+);
+
+app.get("/logout", function(req, res) {
+  req.logout();
+  req.session.destroy();
+  res.redirect("/");
+});
+
+
+
+
+
 const path = require("path")
 app.get("*", (req, res, next) => {
   res.sendFile(path.join(__dirname, "/../build/index.html"))
