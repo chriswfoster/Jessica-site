b79423f0e3eff5d58ef3c2223e72c4f323590719
ChriswFoster <chriswfoster@gmail.com>
2018-02-01 20:26:07 -0600

    Installed cors, session-express, session, set up auth0, and a few endpoints for auth0 setup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require("express")
 const { json } = require("body-parser")
-const massive = require("massive")
+const cors = require("cors");
+const session = require("express-session");
+const massive = require("massive");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+
+const { secret } = require("../config").session;
+const { domain, clientID, clientSecret } = require("../config.js").auth0;
+const { connectionString } = require("../config").massive;
 
 const app = express()
 
