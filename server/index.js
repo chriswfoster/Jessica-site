8e83cfac4f19eb59c2d0b2068b6bfcc970a2d909
ChriswFoster <chriswfoster@gmail.com>
2018-02-01 20:32:41 -0600

    Added empty controller

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 const express = require("express")
 const { json } = require("body-parser")
-const cors = require("cors");
-const session = require("express-session");
-const massive = require("massive");
-const passport = require("passport");
-const Auth0Strategy = require("passport-auth0");
+const cors = require("cors")
+const session = require("express-session")
+const massive = require("massive")
+const passport = require("passport")
+const Auth0Strategy = require("passport-auth0")
 
-const { secret } = require("../config").session;
-const { domain, clientID, clientSecret } = require("../config.js").auth0;
-const { connectionString } = require("../config").massive;
+const { secret } = require("../config").session
+const { domain, clientID, clientSecret } = require("../config.js").auth0
+const { connectionString } = require("../config").massive
 
 const app = express()
 
@@ -17,26 +17,23 @@ app.use(express.static(`${__dirname}/../build`))
 const port = 3001
 app.use(json())
 
-
-
 app.use(
   session({
     secret,
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: false
   })
-);
-
+)
 
 massive(connectionString)
   .then(db => app.set("db", db))
-  .catch(console.log);
+  .catch(console.log)
 
-app.use(json());
-app.use(cors());
+app.use(json())
+app.use(cors())
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
 passport.use(
   new Auth0Strategy(
@@ -56,39 +53,36 @@ passport.use(
               .get("db")
               .createUserByAuth([profile.id, profile.displayName])
               .then(created => {
-                return done(null, created[0]);
-              });
+                return done(null, created[0])
+              })
           } else {
-            return done(null, response[0]);
+            return done(null, response[0])
           }
-        });
+        })
     }
   )
-);
+)
 
 passport.serializeUser(function(user, done) {
-  done(null, user);
-});
+  done(null, user)
+})
 
 passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
+  done(null, obj)
+})
 
-app.get("/login",
+app.get(
+  "/login",
   passport.authenticate("auth0", {
     successRedirect: "/"
   })
-);
+)
 
 app.get("/logout", function(req, res) {
-  req.logout();
-  req.session.destroy();
-  res.redirect("/");
-});
-
-
-
-
+  req.logout()
+  req.session.destroy()
+  res.redirect("/")
+})
 
 const path = require("path")
 app.get("*", (req, res, next) => {
