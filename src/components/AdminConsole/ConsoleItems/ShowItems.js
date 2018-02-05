e1e573174aaea2827b3293424ff82fc461409815
ChriswFoster <chriswfoster@gmail.com>
2018-02-05 12:55:42 -0600

    I think the admin view item tiles are complete

--- src/components/AdminConsole/ConsoleItems/ShowItems.js
+++ src/components/AdminConsole/ConsoleItems/ShowItems.js
@@ -8,8 +8,7 @@ class ShowItems extends Component {
     this.state = {
       itemslist: [],
       itemToRemove: 0,
-      showRemoveModal: "loginNOPE",
-  
+      showRemoveModal: "loginNOPE"
     }
   }
 
@@ -25,21 +24,21 @@ class ShowItems extends Component {
   }
 
   deleteItem() {
-    const {itemToRemove, itemslist} = this.state
-    const temparray = itemslist;
-    axios.post('/api/removeItem', {
+    const { itemToRemove, itemslist } = this.state
+    const temparray = itemslist
+    axios.post("/api/removeItem", {
       item_id: itemToRemove
     })
-    for (var i = 0; i < temparray.length; i++){
-      if (temparray[i].item_id === itemToRemove){
+    for (var i = 0; i < temparray.length; i++) {
+      if (temparray[i].item_id === itemToRemove) {
         temparray.splice(i, 1)
       }
-      this.setState({itemslist: temparray})
+      this.setState({ itemslist: temparray })
     }
     this.closeWindow()
   }
-  closeWindow(){
-    this.setState({showRemoveModal: "loginNOPE"})
+  closeWindow() {
+    this.setState({ showRemoveModal: "loginNOPE" })
   }
 
   render() {
@@ -47,21 +46,40 @@ class ShowItems extends Component {
     const displayList = itemslist.map((item, i) => (
       <div className="adminconsoleitemsorganize showitemstileborder" key={i}>
         {/* This CSS class ^ is found in Shop's css file. */}
-        <div
-          onClick={() => this.removeItem(item.item_id)}
-          className="showitemstileX"
-        >
-          X
+        <div>
+          <p
+            onClick={() => this.removeItem(item.item_id)}
+            className="showitemstileX"
+          >
+            X
+          </p>
+          <img
+            src={item.item_picurl}
+            className="showitemsimages"
+            alt="No Image available"
+          />
+          <table><tr>
+            <th><p style={{ color: "red" }}>Item:</p> 
+              </th>
+              <th><p style={{ color: "red" }}>Price:</p> </th>
+              <th><p style={{ color: "red" }}>Item Id:</p> </th>
+            </tr>
+            <tr>
+              <td><p style={{ color: "white" }}>
+              {item.item_name}
+            </p>
+                </td>
+                <td><p style={{ color: "white" }}>
+       ${item.item_price}
+            </p>
+                  </td>
+                  <td><p>
+           {item.item_id}
+            </p>
+                    </td>
+              </tr>
+          </table>
         </div>
-        <img
-          src={item.item_picurl}
-          className="showitemsimages"
-          alt="No Image available"
-        />
-
-        <p style={{ color: "white" }}><p style={{color: "red"}}>Item:</p> {item.item_name}</p>
-        <p style={{ color: "white" }}><p style={{color: "red"}}>Price:</p> {item.item_price}</p>
-        <p><p style={{color: "red"}}>Item Id:</p> {item.item_id}</p>
       </div>
     ))
     return (
@@ -71,13 +89,11 @@ class ShowItems extends Component {
         </div>
 
         <div className={this.state.showRemoveModal}>
-        <div
-          className={this.state.showModal}
-          >
-          <div> DELETE ITEM ID#: {this.state.questionitem}</div>
-          <button onClick={() => this.deleteItem()}>YES</button>
-          <button onClick={()=> this.closeWindow()}>No</button>
-        </div>
+          <div className={this.state.showModal}>
+            <div> DELETE ITEM ID#: {this.state.questionitem}</div>
+            <button onClick={() => this.deleteItem()}>YES</button>
+            <button onClick={() => this.closeWindow()}>No</button>
+          </div>
         </div>
       </div>
     )
